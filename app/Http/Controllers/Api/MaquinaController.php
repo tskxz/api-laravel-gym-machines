a371cbd9b3d48980cde47fbd7785002cc9c5eccf
Tanjil <tanjilkh@gmail.com>
2024-07-06 17:32:16 +0100

    Create controller, request and resource

---
+++ app/Http/Controllers/Api/MaquinaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Maquina;
+use Illuminate\Http\Request;
+
+class MaquinaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Maquina $maquina)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Maquina $maquina)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Maquina $maquina)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Maquina $maquina)
+    {
+        //
+    }
+}
