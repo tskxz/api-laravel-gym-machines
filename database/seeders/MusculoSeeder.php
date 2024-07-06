99d61325fddd563016ed1c869a6f58c3655c6216
Tanjil <tanjilkh@gmail.com>
2024-07-06 10:58:39 +0100

    Update MusculoSeeder.php

--- database/seeders/MusculoSeeder.php
+++ database/seeders/MusculoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class MusculoSeeder extends Seeder
 {
@@ -12,6 +13,47 @@ class MusculoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('musculos')->insert([
+            'nome' => "Bicep"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Tricep"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Ombro Frontal"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Ombro Lateral"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Ombro Anterior"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Dorsal"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Quadriceps"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Trapézio"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Abdominais"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Antebraço"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Peito"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Isquiotibiais"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Gémeos"
+        ]);
+        DB::table('musculos')->insert([
+            'nome' => "Glúteos"
+        ]);
     }
 }
