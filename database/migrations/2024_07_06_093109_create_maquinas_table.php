8f66a3386ff33d5708f782643926ad369247be9e
Tanjil <tanjilkh@gmail.com>
2024-07-06 10:35:10 +0100

    Update 2024_07_06_093109_create_maquinas_table.php
    adicionar campos na migracao

--- database/migrations/2024_07_06_093109_create_maquinas_table.php
+++ database/migrations/2024_07_06_093109_create_maquinas_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('maquinas', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->text('descricao');
+            $table->boolean('estado')->default(1); // 0 danificado, 1 funcional
             $table->timestamps();
         });
     }
