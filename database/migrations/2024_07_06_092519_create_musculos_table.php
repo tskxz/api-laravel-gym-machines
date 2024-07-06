8c626805cc15d3b70a813e3339079368a35c87e8
Tanjil <tanjilkh@gmail.com>
2024-07-06 10:27:02 +0100

    Update 2024_07_06_092519_create_musculos_table.php

--- database/migrations/2024_07_06_092519_create_musculos_table.php
+++ database/migrations/2024_07_06_092519_create_musculos_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('musculos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
             $table->timestamps();
         });
     }
