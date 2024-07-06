5b926d1bc4fdd0891fba749131cbd3bc9e4f2e40
Tanjil <tanjilkh@gmail.com>
2024-07-06 17:44:12 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\MusculoController;
+use App\Http\Controllers\Api\MaquinaController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('musculos', MusculoController::class)->except([
+Route::apiResource('musculo', MusculoController::class)->except([
     'create', 'edit'
 ]);
+
+Route::apiResource('maquina', MaquinaController::class)->except(['create', 'edit']);
