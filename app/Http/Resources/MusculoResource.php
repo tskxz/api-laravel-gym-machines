a13fb80d49bac10c0a6b7b454e4e6b706a51ac20
Tanjil <tanjilkh@gmail.com>
2024-07-06 11:12:36 +0100

    Update MusculoResource.php

--- app/Http/Resources/MusculoResource.php
+++ app/Http/Resources/MusculoResource.php
@@ -14,6 +14,10 @@ class MusculoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'=>$this->id,
+            'nome'=>$this->nome,
+            'created_at'=>$this->created_at,
+        ];
     }
 }
