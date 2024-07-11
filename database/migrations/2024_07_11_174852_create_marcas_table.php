15639f2030fefd15daf543aed41d42264db1d712
Tanjil <tanjilkh@gmail.com>
2024-07-11 18:50:54 +0100

    Update 2024_07_11_174852_create_marcas_table.php

--- database/migrations/2024_07_11_174852_create_marcas_table.php
+++ database/migrations/2024_07_11_174852_create_marcas_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('marcas', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
             $table->timestamps();
         });
     }
