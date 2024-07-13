28003267015f769c7c775c9b1def83d3c35d18ce
Tanjil <tanjilkh@gmail.com>
2024-07-13 17:14:13 +0100

    Update IsAdmin.php
    implement logic middleware

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
@@ -15,6 +16,10 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if(Auth::check()){
+            return $next($request);
+        }
+        return response()->json(['error' => 'Acesso não autorizado'], 403);
+        
     }
 }
